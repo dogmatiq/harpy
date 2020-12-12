8f78b69b27cf384fd8a41b899a09fa169396d278
James Harris <mailjamesharris@gmail.com>
2020-12-12 16:52:07 +1000

    Refactor `HandlerInvoker` use new `New[Success|Error]Response()` functions.

--- handler.go
+++ handler.go
@@ -2,13 +2,12 @@ package voorhees
 
 import (
 	"context"
-	"errors"
 
 	"github.com/dogmatiq/dodeca/logging"
 )
 
-// Handler is a function that produces a result value in response to a
-// JSON-RPC request.
+// Handler is a function that produces a result value in response to a JSON-RPC
+// request.
 //
 // res is the result value to include in the JSON-RPC response (it is not the
 // JSON-RPC response itself).
@@ -20,7 +19,7 @@ import (
 // always ignored.
 type Handler func(ctx context.Context, req Request) (res interface{}, err error)
 
-// HandlerInvoker is a PipelineStage that dispatches to a Handler.
+// HandlerInvoker is a PipelineStage that invokes a Handler.
 type HandlerInvoker struct {
 	// Handle is the function that handles the request.
 	Handler Handler
@@ -32,110 +31,42 @@ type HandlerInvoker struct {
 // Call handles a call request and returns the response.
 func (i *HandlerInvoker) Call(ctx context.Context, req Request) Response {
 	result, err := i.Handler(ctx, req)
-	if err != nil {
-		return i.buildErrorResponse(req, err)
-	}
 
-	return i.buildSuccessResponse(req, result)
-}
-
-// buildSuccessResponse returns the JSON-RPC response to send after successful
-// handling of a call.
-func (i *HandlerInvoker) buildSuccessResponse(req Request, result interface{}) Response {
-	res, err := NewSuccessResponse(req.ID, result)
-	if err != nil {
-		return i.buildErrorResponse(req, err)
-	}
-
-	logging.Log(
-		i.Logger,
-		`✓ CALL[%s] %s`,
-		req.ID,
-		req.Method,
-	)
-
-	return res
-}
-
-// buildErrorResponse returns the JSON-RPC response to send after a failure to
-// handle a call.
-func (i *HandlerInvoker) buildErrorResponse(req Request, err error) (res ErrorResponse) {
-	if nerr, ok := err.(Error); ok {
-		return i.buildNativeErrorResponse(req, nerr)
-	} else if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) {
-		return i.buildTransparentErrorResponse(req, err)
+	var res Response
+	if err == nil {
+		res = NewSuccessResponse(req.ID, result)
 	} else {
-		return i.buildOpaqueErrorResponse(req, err)
-	}
-}
-
-// buildNativeErrorResponse returns the JSON-RPC response to send when a handler
-// returns a native JSON-RPC Error.
-func (i *HandlerInvoker) buildNativeErrorResponse(req Request, err Error) ErrorResponse {
-	res, rerr := NewErrorResponse(req.ID, err)
-	if rerr != nil {
-		return i.buildOpaqueErrorResponse(req, rerr)
+		res = NewErrorResponse(req.ID, err)
 	}
 
-	logging.Log(
-		i.Logger,
-		`✗ CALL[%s] %s  %s`,
-		req.ID,
-		req.Method,
-		err,
-	)
-
-	return res
-}
-
-// buildOpaqueErrorResponse returns the JSON-RPC response to return when the
-// handling of a request failed but the direct cause should NOT be reported to
-// the caller.
-func (i *HandlerInvoker) buildOpaqueErrorResponse(req Request, err error) ErrorResponse {
-	res := ErrorResponse{
-		Version:   jsonRPCVersion,
-		RequestID: req.ID,
-		Error: ErrorInfo{
-			Code:    InternalErrorCode,
-			Message: InternalErrorCode.String(), // Note, we do NOT use the actual error message in the response.
-		},
+	switch res := res.(type) {
+	case SuccessResponse:
+		logging.Log(
+			i.Logger,
+			`✓ CALL[%s] %s`,
+			req.ID,
+			req.Method,
+		)
+	case ErrorResponse:
+		if res.ServerError != nil {
+			logging.Log(
+				i.Logger,
+				`✗ CALL[%s] %s  %s  [cause: %s]`,
+				req.ID,
+				req.Method,
+				res.Error,
+				res.ServerError,
+			)
+		} else {
+			logging.Log(
+				i.Logger,
+				`✗ CALL[%s] %s  %s`,
+				req.ID,
+				req.Method,
+				res.Error,
+			)
+		}
 	}
-
-	logging.Log(
-		i.Logger,
-		`✗ CALL[%s] %s  [%d] %s: %s  (cause not shown to caller)`,
-		req.ID,
-		req.Method,
-		res.Error.Code,
-		res.Error.Code,
-		err,
-	)
-
-	return res
-}
-
-// buildTransparentErrorResponse returns the JSON-RPC response to return when
-// the handling of a request failed and it is safe to inform the caller of the
-// direct cause.
-func (i *HandlerInvoker) buildTransparentErrorResponse(req Request, err error) ErrorResponse {
-	res := ErrorResponse{
-		Version:   jsonRPCVersion,
-		RequestID: req.ID,
-		Error: ErrorInfo{
-			Code:    InternalErrorCode,
-			Message: err.Error(), // Note, we use the actual error message in the response.
-		},
-	}
-
-	logging.Log(
-		i.Logger,
-		`✗ CALL[%s] %s  [%d] %s: %s`,
-		req.ID,
-		req.Method,
-		res.Error.Code,
-		res.Error.Code,
-		err,
-	)
 
 	return res
 }
--- handler_test.go
+++ handler_test.go
@@ -58,6 +58,19 @@ var _ = Describe("type HandlerInvoker", func() {
 		})
 
 		When("the handler succeeds", func() {
+			var result interface{}
+
+			BeforeEach(func() {
+				result = 456
+
+				stage.Handler = func(
+					context.Context,
+					Request,
+				) (interface{}, error) {
+					return result, nil
+				}
+			})
+
 			It("logs the invocation", func() {
 				stage.Call(context.Background(), request)
 
@@ -69,72 +82,18 @@ var _ = Describe("type HandlerInvoker", func() {
 				))
 			})
 
-			When("the result is nil", func() {
-				It("returns a success response", func() {
-					res := stage.Call(context.Background(), request)
-					Expect(res).To(Equal(SuccessResponse{
-						Version:   `2.0`,
-						RequestID: json.RawMessage(`123`),
-					}))
-				})
-			})
-
-			When("the response is non-nil", func() {
-				var result interface{}
-
-				BeforeEach(func() {
-					result = 456
-
-					stage.Handler = func(
-						context.Context,
-						Request,
-					) (interface{}, error) {
-						return result, nil
-					}
-				})
-
-				It("returns a success response that contains the marshaled result", func() {
-					res := stage.Call(context.Background(), request)
-					Expect(res).To(Equal(SuccessResponse{
-						Version:   `2.0`,
-						RequestID: json.RawMessage(`123`),
-						Result:    json.RawMessage(`456`),
-					}))
-				})
-
-				When("the result can not be marshaled", func() {
-					BeforeEach(func() {
-						result = 10i + 1
-					})
-
-					It("returns an error response", func() {
-						res := stage.Call(context.Background(), request)
-						Expect(res).To(Equal(ErrorResponse{
-							Version:   `2.0`,
-							RequestID: json.RawMessage(`123`),
-							Error: ErrorInfo{
-								Code:    InternalErrorCode,
-								Message: "internal server error",
-							},
-						}))
-					})
-
-					It("logs the invocation", func() {
-						stage.Call(context.Background(), request)
-
-						Expect(logger.Messages()).To(ContainElement(
-							logging.BufferedLogMessage{
-								Message: `✗ CALL[123] <method>  [-32603] internal server error: could not marshal success result value: json: unsupported type: complex128  (cause not shown to caller)`,
-								IsDebug: false,
-							},
-						))
-					})
-				})
+			It("returns a success response that contains the marshaled result", func() {
+				res := stage.Call(context.Background(), request)
+				Expect(res).To(Equal(SuccessResponse{
+					Version:   `2.0`,
+					RequestID: json.RawMessage(`123`),
+					Result:    json.RawMessage(`456`),
+				}))
 			})
 		})
 
-		When("the handler returns a native JSON-RPC error", func() {
-			var err Error
+		When("the handler returns an error", func() {
+			var err error
 
 			BeforeEach(func() {
 				err = NewError(789, WithMessage("<error>"))
@@ -158,176 +117,33 @@ var _ = Describe("type HandlerInvoker", func() {
 				))
 			})
 
-			When("the error does not contain any user-defined data", func() {
-				It("returns an error response", func() {
-					res := stage.Call(context.Background(), request)
-					Expect(res).To(Equal(ErrorResponse{
-						Version:   `2.0`,
-						RequestID: json.RawMessage(`123`),
-						Error: ErrorInfo{
-							Code:    789,
-							Message: "<error>",
-						},
-					}))
-				})
+			It("returns an error response", func() {
+				res := stage.Call(context.Background(), request)
+				Expect(res).To(Equal(ErrorResponse{
+					Version:   `2.0`,
+					RequestID: json.RawMessage(`123`),
+					Error: ErrorInfo{
+						Code:    789,
+						Message: "<error>",
+					},
+				}))
 			})
 
-			When("the error contains user-defined data", func() {
+			When("the error is unrecognised", func() {
 				BeforeEach(func() {
-					err = NewError(
-						789,
-						WithMessage("<error>"),
-						WithData([]int{100, 200, 300}),
-					)
+					err = errors.New("<error>")
 				})
 
-				It("returns an error response containing the user-defined data", func() {
-					res := stage.Call(context.Background(), request)
-					Expect(res).To(Equal(ErrorResponse{
-						Version:   `2.0`,
-						RequestID: json.RawMessage(`123`),
-						Error: ErrorInfo{
-							Code:    789,
-							Message: "<error>",
-							Data:    json.RawMessage(`[100,200,300]`),
+				It("logs the cause of the internal error", func() {
+					stage.Call(context.Background(), request)
+
+					Expect(logger.Messages()).To(ContainElement(
+						logging.BufferedLogMessage{
+							Message: `✗ CALL[123] <method>  [-32603] internal server error  [cause: <error>]`,
+							IsDebug: false,
 						},
-					}))
+					))
 				})
-
-				When("the data can not be marshaled", func() {
-					BeforeEach(func() {
-						err = NewError(
-							789,
-							WithMessage("<error>"),
-							WithData(10i+1),
-						)
-					})
-
-					It("returns an error response", func() {
-						res := stage.Call(context.Background(), request)
-						Expect(res).To(Equal(ErrorResponse{
-							Version:   `2.0`,
-							RequestID: json.RawMessage(`123`),
-							Error: ErrorInfo{
-								Code:    InternalErrorCode,
-								Message: "internal server error",
-							},
-						}))
-					})
-
-					It("logs the invocation", func() {
-						stage.Call(context.Background(), request)
-
-						Expect(logger.Messages()).To(ContainElement(
-							logging.BufferedLogMessage{
-								Message: `✗ CALL[123] <method>  [-32603] internal server error: could not marshal user-defined error data in [789] <error>: json: unsupported type: complex128  (cause not shown to caller)`,
-								IsDebug: false,
-							},
-						))
-					})
-				})
-			})
-		})
-
-		When("the handler returns a context.DeadlineExceeded error", func() {
-			BeforeEach(func() {
-				stage.Handler = func(
-					context.Context,
-					Request,
-				) (interface{}, error) {
-					return nil, context.DeadlineExceeded
-				}
-			})
-
-			It("logs the invocation", func() {
-				stage.Call(context.Background(), request)
-
-				Expect(logger.Messages()).To(ContainElement(
-					logging.BufferedLogMessage{
-						Message: `✗ CALL[123] <method>  [-32603] internal server error: context deadline exceeded`,
-						IsDebug: false,
-					},
-				))
-			})
-
-			It("returns an error response", func() {
-				res := stage.Call(context.Background(), request)
-				Expect(res).To(Equal(ErrorResponse{
-					Version:   `2.0`,
-					RequestID: json.RawMessage(`123`),
-					Error: ErrorInfo{
-						Code:    InternalErrorCode,
-						Message: "context deadline exceeded",
-					},
-				}))
-			})
-		})
-
-		When("the handler returns a context.Canceled error", func() {
-			BeforeEach(func() {
-				stage.Handler = func(
-					context.Context,
-					Request,
-				) (interface{}, error) {
-					return nil, context.Canceled
-				}
-			})
-
-			It("logs the invocation", func() {
-				stage.Call(context.Background(), request)
-
-				Expect(logger.Messages()).To(ContainElement(
-					logging.BufferedLogMessage{
-						Message: `✗ CALL[123] <method>  [-32603] internal server error: context canceled`,
-						IsDebug: false,
-					},
-				))
-			})
-
-			It("returns an error response", func() {
-				res := stage.Call(context.Background(), request)
-				Expect(res).To(Equal(ErrorResponse{
-					Version:   `2.0`,
-					RequestID: json.RawMessage(`123`),
-					Error: ErrorInfo{
-						Code:    InternalErrorCode,
-						Message: "context canceled",
-					},
-				}))
-			})
-		})
-
-		When("the handler returns an unrecognised error", func() {
-			BeforeEach(func() {
-				stage.Handler = func(
-					context.Context,
-					Request,
-				) (interface{}, error) {
-					return nil, errors.New("<error>")
-				}
-			})
-
-			It("logs the invocation", func() {
-				stage.Call(context.Background(), request)
-
-				Expect(logger.Messages()).To(ContainElement(
-					logging.BufferedLogMessage{
-						Message: `✗ CALL[123] <method>  [-32603] internal server error: <error>  (cause not shown to caller)`,
-						IsDebug: false,
-					},
-				))
-			})
-
-			It("returns an error response that does NOT include the causal error message", func() {
-				res := stage.Call(context.Background(), request)
-				Expect(res).To(Equal(ErrorResponse{
-					Version:   `2.0`,
-					RequestID: json.RawMessage(`123`),
-					Error: ErrorInfo{
-						Code:    InternalErrorCode,
-						Message: "internal server error",
-					},
-				}))
 			})
 		})
 	})
