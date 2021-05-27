114d9df02275ad7c6bb84a0de8dbca5bee45e8b8
James Harris <mailjamesharris@gmail.com>
2021-05-28 08:48:37 +1000

    Replace `HandlerInvoker` with `Router`.

--- handler.go
+++ handler.go
@@ -2,12 +2,10 @@ package harpy
 
 import (
 	"context"
-
-	"github.com/dogmatiq/dodeca/logging"
 )
 
 // A Handler is a function that produces a result value (or error) in response
-// to a JSON-RPC request.
+// to a JSON-RPC request for a specific method.
 //
 // res is the result value to include in the JSON-RPC response; it is not the
 // JSON-RPC response itself. If err is non-nil, a JSON-RPC error response is
@@ -17,77 +15,38 @@ import (
 // always ignored.
 type Handler func(ctx context.Context, req Request) (res interface{}, err error)
 
-// HandlerInvoker is an implementation of the Exchanger interface that invokes a
-// Handler.
-//
-// It logs meta-data about each request and response, but does not include the
-// complete data structure for request/response information. Such logging can be
-// useful for debugging, and is provided by the ExchangeLogger type.
-type HandlerInvoker struct {
-	// Handle is the function that handles the request.
-	Handler Handler
-
-	// Logger is the target for messages about the requests and responses.
-	Logger logging.Logger
-}
+// Router is a Exchanger that dispatches to different handlers based on the
+// JSON-RPC method name.
+type Router map[string]Handler
 
 // Call handles a call request and returns the response.
-func (i *HandlerInvoker) Call(ctx context.Context, req Request) Response {
-	result, err := i.Handler(ctx, req)
-
-	var res Response
-	if err == nil {
-		res = NewSuccessResponse(req.ID, result)
-	} else {
-		res = NewErrorResponse(req.ID, err)
-	}
-
-	switch res := res.(type) {
-	case SuccessResponse:
-		logging.Log(
-			i.Logger,
-			`✓ '%s' CALL`,
-			req.Method,
+//
+// It invokes the handler associated with the method specified by the request.
+// If no such method has been registered it returns a JSON-RPC "method not
+// found" error response.
+func (r Router) Call(ctx context.Context, req Request) Response {
+	h, ok := r[req.Method]
+	if !ok {
+		return NewErrorResponse(
+			req.ID,
+			MethodNotFound(),
 		)
-	case ErrorResponse:
-		if res.ServerError != nil {
-			logging.Log(
-				i.Logger,
-				`✗ '%s' CALL  %s  [cause: %s]`,
-				req.Method,
-				res.Error,
-				res.ServerError,
-			)
-		} else {
-			logging.Log(
-				i.Logger,
-				`✗ '%s' CALL  %s`,
-				req.Method,
-				res.Error,
-			)
-		}
 	}
 
-	return res
+	result, err := h(ctx, req)
+	if err != nil {
+		return NewErrorResponse(req.ID, err)
+	}
+
+	return NewSuccessResponse(req.ID, result)
 }
 
 // Notify handles a notification request.
-func (i *HandlerInvoker) Notify(ctx context.Context, req Request) {
-	_, err := i.Handler(ctx, req)
-	if err != nil {
-		logging.Log(
-			i.Logger,
-			`✗ '%s' NOTIFY  %s`,
-			req.Method,
-			err,
-		)
-
-		return
+//
+// It invokes the handler associated with the method specified by the request.
+// If no such method has been registered it does nothing.
+func (r Router) Notify(ctx context.Context, req Request) {
+	if h, ok := r[req.Method]; ok {
+		h(ctx, req) // nolint:errcheck // notification errors are not reported to the caller
 	}
-
-	logging.Log(
-		i.Logger,
-		`✓ '%s' NOTIFY`,
-		req.Method,
-	)
 }
